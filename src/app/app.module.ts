8256bbe70fad9d74856ca404b870db52bbaa82bf
Kyle Cordes <kyle.cordes@oasisdigital.com>
2018-03-22 09:06:14 -0500

    curriculum update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
-const routes: Routes = [
-  { path: '', redirectTo: 'employees', pathMatch: 'full' },
-  { path: 'employees', loadChildren: './employee/employee.module#EmployeeModule' }
-];
 
 @NgModule({
   declarations: [
